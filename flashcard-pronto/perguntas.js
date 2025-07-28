93a206d2b8e433e9a0b3770cdc560b227ce12228
Friske <gustavo.friske.avila@escola.pr.gov.br>
2025-07-27 21:23:04 -0300

    Add files via upload

---
+++ flashcard-pronto/perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Handebol',
+    ' Quantos jogadores tem em uma partida de handebol?',
+    'Em uma partida de handebol, cada equipe é composta por 14 jogadores, sendo que 7 deles podem estar em quadra simultaneamente. Desses 7, um é o goleiro e os outros 6 são jogadores de linha..'
+)
+
+criaCartao(
+    'Matematica',
+    'Quantos numeros primos temos de 1 a 25? ',
+    'Existem 9 números primos no intervalo de 1 a 25.'
+)
+
+criaCartao(
+    'Séries',
+    'Quais são as 5 series mais assitidas da netflix?',
+    'As cinco séries mais assistidas da Netflix, até agora, são: Wandinha, Stranger Things (4ª temporada), Dahmer: Um Canibal Americano, Bridgerton (1ª temporada) e O Gambito da Rainha'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual é o maior oceano do planeta Terra?',
+    'O maior oceano do planeta é o Oceano Pacífico. Ele cobre aproximadamente 30% da superfície terrestre e é maior do que toda a área terrestre combinada.  '
+)
+
+criaCartao(
+    'História',
+    'Em que ano ocorreu a queda do Muro de Berlim?',
+    ' A queda do Muro de Berlim ocorreu em 1989, marcando o fim simbólico da Guerra Fria e o início da reunificação da Alemanha.'
+)
